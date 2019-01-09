09bd678dc561b60a52f7197f8ef4d5eb27275e72
darenlarson <larson.daren@gmail.com>
2019-01-09 17:35:42 -0600

    Added logic to get route for specific id so that it handles the event of the client entering an id that doesn't exist in the database. Also added a .catch for errors.

--- api/server.js
+++ api/server.js
@@ -30,14 +30,25 @@ server.get('/users', (req, res) => {
         .then(users => {
             res.status(200).json(users);
         })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be retrieved." });
+        });
 });
 
 server.get('/users/:id', (req, res) => {
     const id = req.params.id;
     userDb.get(id)
         .then(user => {
-            res.status(200).json(user);
+            if(user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist" });
+            }
         })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be retrieved." })
+        })
+
 });
 
 server.post('/users', toUpper, (req, res) => {
