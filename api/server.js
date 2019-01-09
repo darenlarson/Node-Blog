6ccfab77840dfe68e14d2493e9e9f1306787a5d0
darenlarson <larson.daren@gmail.com>
2019-01-09 16:11:32 -0600

    routes for get all users, get individual user, and post for a user are functional

--- api/server.js
+++ api/server.js
@@ -1,12 +1,43 @@
 const express = require('express');
+const userDb = require('../data/helpers/userDb.js');
+const tagDb = require('../data/helpers/tagDb.js');
+const postDb = require('../data/helpers/postDb.js');
+
 const server = express();
 
-
 // middleware
-server.get('/', (req, res) => {
-    res.send('server alive');
-});
+server.use(express.json());
+
+function toUpper(req, res, next) {
+    const name = req.body.name
+
+    const upperName = name.toUpperCase();
+    next();
+}
 
 // routes
+server.get('/users', (req, res) => {
+    userDb.get()
+        .then(users => {
+            res.status(200).json(users);
+        })
+});
+
+server.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    userDb.get(id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+});
+
+server.post('/users', (req, res) => {
+    const userInfo = req.body;
+
+    userDb.insert(userInfo)
+        .then(idObject => {
+            res.status(201).json(idObject);
+        })
+});
 
 module.exports = server;
